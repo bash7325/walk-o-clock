9f80ca2a67676c540bae3e550543e18118ac35fc
Brandon Ashby <bashby@corp.justiceworks.com>
2023-04-20 14:50:35 -0600

    small css changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,9 @@ export class AppComponent {
   selectedUser: string = '';
   weatherData: any;
   recommendation: string = '';
-  icon: string = '';
+  weatherIcon: string = '';
+  walkIcon: string = '';
+
 
   private apiKey: string = '20d3501773a839af3857d2b0374101f6';
   private apiUrl: string = 'https://api.openweathermap.org/data/2.5/weather';
@@ -37,15 +39,28 @@ export class AppComponent {
           // Set the recommendation message based on the user and weather criteria
           if (this.selectedUser === 'Alex' && this.weatherData.main.temp >= 60 && this.weatherData.main.temp <= 79) {
             this.recommendation = "It's Walk-O-Clock!";
+            this.walkIcon = '../assets/icons/icons8-walking-64.png'
           } else if (this.selectedUser === 'Corey' && this.weatherData.main.temp >= 70 && this.weatherData.main.temp <= 79) {
             this.recommendation = "Get up Corey, It's Walk-O-Clock!";
+            this.walkIcon = '../assets/icons/icons8-walking-64.png'
           } else if (this.selectedUser === 'Not a weather wimp') {
             this.recommendation = "It's always Walk-O-Clock!";
+            this.walkIcon = '../assets/icons/icons8-walking-64.png'
           } else {
             this.recommendation = "It's NOT Walk-O-Clock";
+            this.walkIcon = '../assets/icons/icons8-armchair-64.png'
           }
-          if (this.weatherData.weather[0].description === 'broken clouds') {
-            this.icon = '../assets/icons/icons8-cloud-32.png';
+          if (this.weatherData.weather[0].description.includes('clouds')) {
+            this.weatherIcon = '../assets/icons/icons8-cloud-32.png';
+          }
+          if (this.weatherData.weather[0].description.includes('rain')) {
+            this.weatherIcon = '../assets/icons/icons8-rain-32.png';
+          }
+          if (this.weatherData.weather[0].description.includes('haze')) {
+            this.weatherIcon = '../assets/icons/icons8-haze-32.png';
+          }
+          if (this.weatherData.weather[0].description.includes('storm')) {
+            this.weatherIcon = '../assets/icons/icons8-storm-32.png';
           }
         }, error => {
           console.error('Error fetching weather data:', error);
