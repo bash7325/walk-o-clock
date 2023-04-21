971172a515d7920d41e1f59cef99e89133f19cb1
Brandon Ashby <bash7325@gmail.com>
2023-04-20 20:02:00 -0600

    fixing paths for gh pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -36,31 +37,30 @@ export class AppComponent {
           this.weatherData = data;
           console.log(this.weatherData);
   
-          // Set the recommendation message based on the user and weather criteria
           if (this.selectedUser === 'Alex' && this.weatherData.main.temp >= 60 && this.weatherData.main.temp <= 79) {
             this.recommendation = "It's Walk-O-Clock!";
-            this.walkIcon = '../assets/icons/icons8-walking-64.png'
+            this.walkIcon = 'assets/icons/icons8-walking-64.png'
           } else if (this.selectedUser === 'Corey' && this.weatherData.main.temp >= 70 && this.weatherData.main.temp <= 79) {
             this.recommendation = "Get up Corey, It's Walk-O-Clock!";
-            this.walkIcon = '../assets/icons/icons8-walking-64.png'
+            this.walkIcon = 'assets/icons/icons8-walking-64.png'
           } else if (this.selectedUser === 'Not a weather wimp') {
             this.recommendation = "It's always Walk-O-Clock!";
-            this.walkIcon = '../assets/icons/icons8-walking-64.png'
+            this.walkIcon = 'assets/icons/icons8-walking-64.png'
           } else {
             this.recommendation = "It's NOT Walk-O-Clock";
-            this.walkIcon = '../assets/icons/icons8-armchair-64.png'
+            this.walkIcon = 'assets/icons/icons8-armchair-64.png'
           }
           if (this.weatherData.weather[0].description.includes('clouds')) {
-            this.weatherIcon = '../assets/icons/icons8-cloud-32.png';
+            this.weatherIcon = 'assets/icons/icons8-cloud-32.png';
           }
           if (this.weatherData.weather[0].description.includes('rain')) {
-            this.weatherIcon = '../assets/icons/icons8-rain-32.png';
+            this.weatherIcon = 'assets/icons/icons8-rain-32.png';
           }
           if (this.weatherData.weather[0].description.includes('haze')) {
-            this.weatherIcon = '../assets/icons/icons8-haze-32.png';
+            this.weatherIcon = 'assets/icons/icons8-haze-32.png';
           }
           if (this.weatherData.weather[0].description.includes('storm')) {
-            this.weatherIcon = '../assets/icons/icons8-storm-32.png';
+            this.weatherIcon = 'assets/icons/icons8-storm-32.png';
           }
         }, error => {
           console.error('Error fetching weather data:', error);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms'; // Import FormsModule
+import { FormsModule } from '@angular/forms'; 
 
 import { AppComponent } from './app.component';
+import { APP_BASE_HREF } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -12,9 +13,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule // Add FormsModule to the imports array
+    FormsModule 
   ],
-  providers: [],
+  providers: [
+    { provide: APP_BASE_HREF, useValue: '!' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
