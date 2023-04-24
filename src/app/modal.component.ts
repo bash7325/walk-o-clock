982ffaa45afab754491b641919365cb6bf80c426
Brandon Ashby <bashby@corp.justiceworks.com>
2023-04-24 08:20:18 -0600

    moved css to global style file

--- src/app/modal.component.ts
+++ src/app/modal.component.ts
@@ -11,28 +11,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
         <button class="nes-btn is-error" (click)="hideModal()">Close</button>
       </div>
     </div>
-  `,
-  styles: [`
-    .modal-wrapper {
-      position: fixed;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      background-color: rgba(0, 0, 0, 0.5);
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      z-index: 9999;
-    }
-
-    .modal {
-      background-color: #fff;
-      padding: 20px;
-      border-radius: 10px;
-      box-shadow: 0 2px 8px rgba(0, 0, 0, 0.25);
-    }
-  `]
+  `
 })
 export class AlertModalComponent {
   @Input() title: string = '';
